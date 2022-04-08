fb089bf74ceadbb8904354cbb0f13f8cf85b0eae
João Pedro da Silva <joao.pedro.da.silva@hotmail.com>
2022-04-07 21:46:08 -0300

    made it responsive

--- script.js
+++ script.js
@@ -9,16 +9,18 @@ req.onload = () => {
   
   createCanvas()
   createScales(dataSet)
-  createAxes(dataSet)
+  createAxes()
+  createTitle()
+  createLabels()
   createCells(dataSet)
-  createLegend(dataSet)
+  createLegend(dataSet)  
 }
 req.send()
 
 const colors = ['#313695', '#4575b4', '#74add1', '#abd9e9', '#e0f3f8', '#ffffbf', '#fee090', '#fdae61', '#f46d43', '#d73027', '#a50026']
-const w = 700
-const h = 550
-const pad = 60
+const w = 600
+const h = 520
+const pad = 80
 let xScale
 let yScale
 let temperatureScale
@@ -26,9 +28,10 @@ let minimumYear
 let maximumYear
 
 const createCanvas = () => {
-  d3.select('#heatmap')
-    .attr('width', w)
-    .attr('height', h)
+  d3.select('.d3-content')
+    .attr("preserveAspectRatio", "xMinYMin meet")
+    .attr("viewBox", `0 0 ${w} ${h}`)
+    .classed("svg-content", true)
 }
 
 const createScales = (dataSet) => {
@@ -50,20 +53,18 @@ const createScales = (dataSet) => {
                        .range([0, colors.length])
 }
 
-const createAxes = (dataSet) => {
+const createAxes = () => {
   const xAxis = d3.axisBottom(xScale)
                   .tickFormat(d3.format('d'))
-                d3.select('#heatmap')
+                d3.select('.d3-content')
                   .append('g')
-                  .attr('id', 'x-axis')
                   .attr('transform', 'translate(0, ' + (h - pad) + ')')
                   .call(xAxis)
   
   const yAxis = d3.axisLeft(yScale)
                   .tickFormat(d3.timeFormat('%B'))                  
-                d3.select('#heatmap')
+                d3.select('.d3-content')
                   .append('g')
-                  .attr('id', 'y-axis')
                   .attr('transform', 'translate(' + (pad) + ',0)')
                   .call(yAxis)
 }
@@ -71,18 +72,18 @@ const createAxes = (dataSet) => {
 const createCells = (dataSet) => {
   const cellWidth = (w - 2 * pad) / (maximumYear - minimumYear)
   const cellHeight = (h - 2 * pad) / 12
-  const tooltip = document.querySelector('#tooltip')
+  const tooltip = document.querySelector('.tooltip')
   const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+  const iframeContainer = document.querySelector('.iframe-container')
+  const tooltipLeft = window.getComputedStyle(iframeContainer).width.replace('px', '')
+  const tooltipBottom = window.getComputedStyle(iframeContainer).height.replace('px', '')
   
-  d3.select('#heatmap')
+  d3.select('.d3-content')
     .selectAll('rect')
     .data(dataSet)
     .enter()
     .append('rect')
     .attr('class', 'cell')
-    .attr('data-month', d => d.month - 1)
-    .attr('data-year', d => d.year)
-    .attr('data-temp', d => d.temp)
     .attr('width', cellWidth)
     .attr('height', cellHeight)
     .attr('x', d => xScale(d.year))
@@ -90,9 +91,8 @@ const createCells = (dataSet) => {
     .attr('fill', d => colors[Math.floor(temperatureScale(d.temp))])
     .on('mouseover', (e, d, i) => {
       tooltip.classList.add('visible')
-      tooltip.setAttribute('data-year', d.year)
-      tooltip.style.left = e.pageX + 'px'
-      tooltip.style.top = e.pageY + 'px'
+      tooltip.style.left = e.clientX - (tooltipLeft * 0.20) + 'px'
+      tooltip.style.top = e.clientY + (tooltipBottom * 0.05) + 'px'
       tooltip.innerHTML = (`
         <p>${d.year} - ${months[d.month - 1]}</p>
         <p>Temperature: ${d.temp.toFixed(3)}°C</p>
@@ -101,15 +101,53 @@ const createCells = (dataSet) => {
     .on('mouseout', () => tooltip.classList.remove('visible'))
 }
 
+const createTitle = () => {
+  d3.select('svg')
+  .append("text")
+  .attr("x", w / 2)
+  .attr("y", pad - 30)
+  .attr("text-anchor", "middle")
+  .style("font-size", "24px")
+  .text("Monthly Global Land-Surface Temperature")
+  
+  d3.select('svg')
+  .append("text")
+  .attr("x", w / 2)
+  .attr("y", pad - 10)
+  .attr("text-anchor", "middle")
+  .style("font-size", "18px")
+  .text("1753 - 2015: base temperature 8.66℃")
+}
+
+const createLabels = () => {
+  //labels   
+  d3.select('svg')
+  .append("text")
+  .attr("transform", "translate(" + (w / 2) + " ," + (h - 35) + ")")
+  .style("text-anchor", "middle")
+  .style("font-size", "16px")
+  .text("Year");
+  
+  d3.select('svg')
+  .append("text")
+  .attr("transform", "rotate(-90)")
+  .attr("x", -(h / 2))
+  .attr("y", 15)
+  .style("text-anchor", "middle")
+  .style("font-size", "16px")
+  .text("Month");
+}
+
 const createLegend = (dataSet) => {
-  const legendWidth = 400
+  const legendWidth = 600
   const legendHeight = 70  
   const legendPad = 30
   const legendRectWidth = legendWidth / colors.length
   
-  d3.select('#legend')
-    .attr('width', legendWidth)
-    .attr('height', legendHeight) 
+  d3.select('.legend')
+  .attr("preserveAspectRatio", "xMinYMin meet")
+  .attr("viewBox", `0 0 ${legendWidth} ${legendHeight}`)
+  .classed("svg-content", true)
   
   const legendXScale = d3.scaleLinear()
                        .domain([d3.min(dataSet, d => d.temp),
@@ -118,12 +156,13 @@ const createLegend = (dataSet) => {
   
   const legendXAxis = d3.axisBottom(legendXScale)
                         .tickFormat(d3.format('.1f'))
-                      d3.select('#legend')
+                      d3.select('.legend')
                         .append('g')
+                        .style("font-size", "10px")
                         .attr('transform', 'translate(0, ' + (legendHeight - legendPad) + ')')
                         .call(legendXAxis)
   
-  d3.select('#legend')
+  d3.select('.legend')
     .selectAll('rect')
     .data(colors)
     .enter()
